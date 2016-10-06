f09a9b7537ddf36ffbe90029b3f399329358bd17
Juan Carlos Mercado <barkatu.83@gmail.com>
2016-10-06 15:33:26 -0500

    Adds country specs

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Country, type: :model do
   let(:country) { FactoryGirl.build(:country) }
   subject { country }
 
+  context 'attributes' do
+    it { should respond_to(:name) }
+  end
+
   context 'validations' do
     it { should validate_presence_of(:name) }
     it { should be_valid }
